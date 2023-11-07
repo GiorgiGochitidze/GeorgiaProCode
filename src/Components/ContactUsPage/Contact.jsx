53e9f25133dae4d60e82a94c702aefd45438d5a5
GiorgiGochitidze <113135317+GiorgiGochitidze@users.noreply.github.com>
2023-11-07 20:13:28 +0400

    modified:   src/Components/CSS/ContactUsCss/contact.css 	modified:   src/Components/ContactUsPage/Contact.jsx

--- src/Components/ContactUsPage/Contact.jsx
+++ src/Components/ContactUsPage/Contact.jsx
@@ -3,14 +3,14 @@ import "../CSS/ContactUsCss/contact.css";
 const Contact = () => {
   return (
     <div className="contact-form-container">
-      <form className="form">
+      <form action="https://formsubmit.co/georgiaprocode@gmail.com" method="POST" className="form">
         <p className="form-title">კონტაქტი</p>
         <div className="input-container">
-          <input type="email" placeholder="თქვენი ემაილი" />
+          <input type="email" name="email" placeholder="თქვენი ემაილი" required />
           <span></span>
         </div>
         <div className="input-container">
-          <input type="text" placeholder="დაწერეთ ტექსტი" />
+          <textarea className="textarea" type="text" name="text" placeholder="დაწერეთ ტექსტი" required />
         </div>
         <button type="submit" className="submit">
           გაგზავნა
