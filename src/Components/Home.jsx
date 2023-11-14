3236e3ad86d0755e6a6c720d9bf0a999862e335d
GiorgiGochitidze <113135317+GiorgiGochitidze@users.noreply.github.com>
2023-11-14 20:40:50 +0400

    deleted:    src/Components/CSS/mainpagecss/aboutUsInMain.css 	modified:   src/Components/Home.jsx 	deleted:    src/Components/mainPageItems/AboutUs.jsx

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,4 +1,3 @@
-import AboutUs from './mainPageItems/AboutUs';
 import WhatWeDo from './mainPageItems/WhatWeDo';
 import CourseLinks from './mainPageItems/CourseLinks'
 import Footer from './mainPageItems/Footer';
@@ -6,8 +5,6 @@ import Footer from './mainPageItems/Footer';
 const Home = () => {
     return ( 
         <div style={{display: 'flex', flexDirection: 'column', gap: '20px', justifyContent: 'center', alignItems: 'center'}}>
-
-            <AboutUs />
 
             <WhatWeDo />
 
