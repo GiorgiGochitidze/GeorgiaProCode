ee8ff41637595983947f18b967c1fcb68a75ba0d
GiorgiGochitidze <113135317+GiorgiGochitidze@users.noreply.github.com>
2023-11-14 19:32:54 +0400

    modified:   src/Components/CSS/mainpagecss/Navbar.css 	modified:   src/Components/Navbar.jsx 	new file:   src/assets/main.png

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import "./CSS/mainpagecss/Navbar.css";
+import main from '../assets/main.png'
 
 const Navbar = ({handleItemClick}) => {
     
@@ -11,6 +12,7 @@ const Navbar = ({handleItemClick}) => {
             <h3>GeorgiaProCode</h3>
           </div>
 
+        <>
           <Link to='/' style={{textDecoration: 'none', color: 'black'}}><p>მთავარი</p></Link>
           <Link to='/Courses' style={{textDecoration: 'none', color: 'black'}}><p>კურსები</p></Link>
           <Link to='/ContactUs' style={{textDecoration: 'none', color: 'black'}}><p>კონტაქტი</p></Link>
@@ -18,7 +20,24 @@ const Navbar = ({handleItemClick}) => {
           <p>რეგისტრაცია</p>
 
           <button onClick={handleItemClick} className="menu-button">მენიუ</button>
+        </>
+
         </nav>
+
+                      {/* in main page navbars under components */}
+        <div className="searchAndImage-container">
+          <div className="content-container">
+              <h1>საუკეთესო კურსები შენთვის და შენი მეგობრებისთვის ქართულად</h1>
+              <p>სახალისო და მარტივი კურსების ნახვა შეგიძლია აქ</p>
+
+              <div className="searchbar-container">
+                <input className="searchBar-input" placeholder="კურსების ძიება..." type="text" />
+                <button className="searchBtn">ძიება</button>
+              </div>
+          </div>
+                      {/* image of girl in navbar */}
+          <img src={main} alt="girl image" />
+        </div>
       </header>
   );
 };
